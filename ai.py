1a3bf717eae7ce8272b39003d4889ad8b0ca670a
quentinlautischer <quentinlautischer@quentinlautischer-VirtualBox.(none)>
2014-04-07 13:13:24 -0600

    s

--- ai.py
+++ ai.py
@@ -3,7 +3,7 @@ import pygame, engine
 class AI(object):
 	"""
 	This object in creted within any unit needing AI controls
-	self.method_sequence is a list of unit methods that will be executed 
+	self.sequence is a list of unit methods that will be executed 
 	sequencially.
 
 	"""
@@ -11,7 +11,7 @@ class AI(object):
 	def __init__(self, unit, sequence_list):
 		self.unit = unit
 		self.set_time = 0
-		self.method_sequence = sequence_list #List of Unit Methods.
+		self.sequence = sequence_list #List of Unit Methods.
 		self.seq_count = 0
 
 	def seq_execute(self, seq_num):
@@ -26,9 +26,9 @@ class AI(object):
 			self.set_time = pygame.time.get_ticks()
 
 		if pygame.time.get_ticks() > self.set_time + SEQ_TIMER:
-			self.method_sequence[seq_num][self.seq_count]()
+			self.sequence[seq_num][self.seq_count]()
 			self.seq_count += 1
-			self.seq_count %= len(self.method_sequence[seq_num])
+			self.seq_count %= len(self.sequence[seq_num])
 			self.set_time = 0
 
 	def find_closest_player(self):
